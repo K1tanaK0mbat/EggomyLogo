967c552e1a115776de44de2d484192424e29fd86
K1tanaK0mbat <kkeayouth@gmail.com>
2023-09-29 12:17:30 -0500

    added apps and inquirer

---
+++ index.js
@@ -0,0 +1,13 @@
+const inquirer = require('inquirer');
+
+inquirer
+.prompt (
+    {
+        type:'input',
+        name:'text',
+        message:'Enter 3 letters for logo', 
+    },
+    {
+
+    }
+)
