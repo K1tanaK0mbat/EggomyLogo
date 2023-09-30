400fcc86cea4d6027955fb277b393dba3090d317
K1tanaK0mbat <kkeayouth@gmail.com>
2023-09-30 02:48:09 -0500

    made inquier questions

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ inquirer
     {
         type:'input',
         name:'text',
-        message:'Enter 3 letters for logo', 
+        message:'Enter 3 letters for logo' 
     },
     {
-
-    }
+        type:'input',
+        name:'textCol',
+        message:'Choose text color'
+    },
+    {
+    type:'list',
+    name:'shape',
+    message:'Choose a shape'
+} ,
+{
+    type:'input',
+    name:'shapeCol',
+    message:'Choose the color for the shape'
+}
 )
