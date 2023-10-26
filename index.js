843278048129f77e5512071e50c7133c5d8bd482
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-25 21:49:31 -0500

    created new objects

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const inquirer = require('inquirer');
 const Triangle = require('./lib/Triangle');
 const Square = require('./lib/Square');
 const Circle = require('./lib/Circle');
+const {writeFile} = require('fs');
+const {createDocument} = require('./lib/svg');
+
+const text = [];
+const textCol = [];
+const color = [];
 
 inquirer
-.prompt (
+.prompt([
     {
         type:'input',
         name:'text',
@@ -13,17 +19,31 @@ inquirer
     {
         type:'input',
         name:'textCol',
-        message:'Choose text color'
+        message:'For text color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color'
     },
     {
-    type:'list',
-    name:'shape',
-    message:'Choose a shape',
-    choices: []
-} ,
-{
-    type:'input',
-    name:'shapeCol',
-    message:'Choose the color for the shape'
-}
-)
+        type:'list',
+        name:'shape',
+        message:'Choose a shape',
+        choices: [Triangle, Square, Circle]
+    },
+    {
+        type:'input',
+        name:'color',
+        message:'For shape color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color'
+    }
+])
+.then(({text, textCol, color, shape}) => {
+    text.push(text);
+    textCol.push(textCol);
+    color.push(color);
+    if (shape === Triangle) {
+        new Triangle(text, textCol, color).render();
+    } else if (shape === Square) {
+        new Square(text, textCol, color).render();
+    } else if (shape === Circle) {
+        new Circle(text, textCol, color).render();
+    }
+})
+.then(() => writeFile('logo.svg', createDocument(shape)))
+.catch(err => console.log(err));
