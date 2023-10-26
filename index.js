ea370c8033dc5eef37a12438f581b7369c07f4ba
K1tanaK0mbat <kkeayouth@gmail.com>
2023-10-25 22:12:53 -0500

    attempt to debug

--- index.js
+++ index.js
@@ -1,49 +1,53 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const { writeFile } = require('fs');
 const Triangle = require('./lib/Triangle');
 const Square = require('./lib/Square');
 const Circle = require('./lib/Circle');
-const {writeFile} = require('fs');
-const {createDocument} = require('./lib/svg');
 
-const text = [];
-const textCol = [];
-const color = [];
+
 
 inquirer
-.prompt([
+  .prompt([
     {
-        type:'input',
-        name:'text',
-        message:'Enter 3 letters for logo' 
+      type: 'input',
+      name: 'text',
+      message: 'Enter 3 letters for logo',
     },
     {
-        type:'input',
-        name:'textCol',
-        message:'For text color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color'
+      type: 'input',
+      name: 'textCol',
+      message: 'For text color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color',
     },
     {
-        type:'list',
-        name:'shape',
-        message:'Choose a shape',
-        choices: [Triangle, Square, Circle]
+      type: 'list',
+      name: 'shape',
+      message: 'Choose a shape',
+      choices: ['Triangle', 'Square', 'Circle'],
     },
     {
-        type:'input',
-        name:'color',
-        message:'For shape color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color'
-    }
+      type: 'input',
+      name: 'color',
+      message: 'For shape color, enter keyword or hex value from here https://developer.mozilla.org/en-US/docs/Web/CSS/named-color',
+    },
 ])
-.then(({text, textCol, color, shape}) => {
-    text.push(text);
-    textCol.push(textCol);
-    color.push(color);
-    if (shape === Triangle) {
-        new Triangle(text, textCol, color).render();
-    } else if (shape === Square) {
-        new Square(text, textCol, color).render();
-    } else if (shape === Circle) {
-        new Circle(text, textCol, color).render();
+  .then(({ text, textCol, color, shapeChoice }) => {
+    this.text = text;
+    this.textCol = textCol;
+    this.color = color;
+    shape = shapeChoice;
+
+    let shapeObject;
+    if (shape === 'Triangle') {
+      shapeObject = new Triangle(this.text, this.textCol, this.color);
+    } else if (shape === 'Square') {
+      shapeObject = new Square(this.text, this.textCol, this.color);
+    } else if (shape === 'Circle') {
+      shapeObject = new Circle(this.text, this.textCol, this.color);
     }
-})
-.then(() => writeFile('logo.svg', createDocument(shape)))
-.catch(err => console.log(err));
+    return shapeObject;
+  })
+  .then(() => {
+    writeFile('logo.svg', this.shapeObject.render() ); 
+  })
+  .catch((err) => console.log(err));
